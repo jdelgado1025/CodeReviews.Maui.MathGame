a4fe730724eb1f709394f4023259015f3aa4586a
COANAHEIM\JDelgado <jdelgado@anaheim.net>
2025-05-06 14:45:46 -0700

    Updated button layout for Main page and default button behavior

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace MathGame.Maui;
+﻿using MathGame.Maui.Views;
+
+namespace MathGame.Maui;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+    }
+
+    private void OnGameChosen(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        Navigation.PushAsync(new Game(btn.Text));
+    }
+    
+    private void OnViewGamesHistory(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new GameHistory());
     }
 }
 
