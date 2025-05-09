b74ae27456e923af9aae87e3cf9bb36df9abebed
COANAHEIM\JDelgado <jdelgado@anaheim.net>
2025-05-09 15:32:16 -0700

    Added new Menu page to display Game options after choosing Difficulty

---
+++ Views/Menu.xaml.cs
@@ -0,0 +1,35 @@
+using MathGame.Maui.Models;
+
+namespace MathGame.Maui.Views;
+
+public partial class Menu : ContentPage
+{
+    public Difficulty Level { get; set; }
+
+    public Menu(Difficulty level)
+	{
+		InitializeComponent();
+        Level = level;
+	}
+
+    private void OnGameChosen(object sender, EventArgs e)
+    {
+        Button btn = (Button)sender;
+        var gameType = btn.Text switch
+        {
+            "Addition" => Operation.Addition,
+            "Subtraction" => Operation.Subtraction,
+            "Multiplication" => Operation.Multiplication,
+            "Division" => Operation.Division,
+            "Random" => Operation.Random,
+            _ => Operation.Random
+        };
+
+        Navigation.PushAsync(new Game(gameType, Level));
+    }
+
+    private void OnViewGamesHistory(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new GameHistory());
+    }
+}
