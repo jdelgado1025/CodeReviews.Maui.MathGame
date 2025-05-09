9ea6d9da33ec195b338bd727ed01615346674075
COANAHEIM\JDelgado <jdelgado@anaheim.net>
2025-05-09 09:59:52 -0700

    Added enum for Operation type & updated to reference

--- Views/Game.xaml.cs
+++ Views/Game.xaml.cs
@@ -1,13 +1,23 @@
+using MathGame.Maui.Models;
+
 namespace MathGame.Maui.Views;
 
 public partial class Game : ContentPage
 {
-    public string GameType { get; set; }
-    public Game(string gameType)
+    public Operation GameType { get; set; }
+
+    public Game(Operation gameType)
 	{
 		InitializeComponent();
 		GameType = gameType;
 		BindingContext = this;
+
+		GenerateQuestion();
+	}
+
+	private void GenerateQuestion()
+	{
+
 	}
 
 	private void OnAnswerSubmitted(object sender, EventArgs e)
