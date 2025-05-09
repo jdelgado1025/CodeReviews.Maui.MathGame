4890173fcab30d21ebb5df9d09cc222c1c25414c
COANAHEIM\JDelgado <jdelgado@anaheim.net>
2025-05-09 15:51:55 -0700

    Added logic to generate Question based off Game and Difficulty

--- Views/Game.xaml.cs
+++ Views/Game.xaml.cs
@@ -14,8 +14,78 @@ public partial class Game : ContentPage
         Level = level;
 		BindingContext = this;
 
-		//GenerateQuestion();
-	}
+        GenerateQuestion();
+    }
+
+    private void GenerateQuestion()
+    {
+        var (numberOne, numberTwo) = GenerateNumbers(GameType, Level);
+
+        var operationSymbol = GameType switch
+        {
+            Operation.Addition => "+",
+            Operation.Subtraction => "-",
+            Operation.Multiplication => "*",
+            Operation.Division => "/"
+        };
+
+        QuestionLabel.Text = $"{numberOne} {operationSymbol} {numberTwo}";
+    }
+
+    private (int, int) GenerateNumbers(Operation gameType, Difficulty difficultyLevel)
+    {
+        int numberOne, numberTwo;
+        int upperLimit;
+
+        const int lowerLimit = 0, divisorLowerLimit = 1;
+        Random rand = new Random();
+
+        switch (difficultyLevel)
+        {
+            case Difficulty.Easy:
+                upperLimit = 100;
+                break;
+            case Difficulty.Medium:
+                upperLimit = 300;
+                break;
+            case Difficulty.Hard:
+                upperLimit = 500;
+                break;
+            case Difficulty.Extreme:
+                upperLimit = 5000;
+                break;
+            default:
+                upperLimit = 100;
+                break;
+        }
+
+        if (gameType == Operation.Division)
+        {
+            /* Ensure the divisor is not 0 and result is a whole number */
+            do
+            {
+                numberOne = rand.Next(lowerLimit, upperLimit);
+                numberTwo = rand.Next(divisorLowerLimit, upperLimit);
+            }
+            while (numberOne % numberTwo != 0);
+        }
+        else
+        {
+            numberOne = rand.Next(lowerLimit, upperLimit);
+            numberTwo = rand.Next(lowerLimit, upperLimit);
+        }
+
+        //Avoid negative number answers & higher number on the right side for Easy level
+        if (difficultyLevel == Difficulty.Easy && numberTwo > numberOne)
+        {
+            int temp = numberOne;
+            numberOne = numberTwo;
+            numberTwo = temp;
+        }
+
+        /* Return two values using a Tuple */
+        return (numberOne, numberTwo);
+    }
 
     private void OnAnswerSubmitted(object sender, EventArgs e)
     {
