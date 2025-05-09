3cd0e5989207ebe58c0846dff2f80c4235803a50
COANAHEIM\JDelgado <jdelgado@anaheim.net>
2025-05-09 15:48:39 -0700

    Barebones Game page to get to from the Menu page

--- MathGame.Maui/Views/Game.xaml.cs
+++ MathGame.Maui/Views/Game.xaml.cs
@@ -5,22 +5,19 @@ namespace MathGame.Maui.Views;
 public partial class Game : ContentPage
 {
     public Operation GameType { get; set; }
+    public Difficulty Level { get; set; }
 
-    public Game(Operation gameType)
+    public Game(Operation gameType, Difficulty level)
 	{
 		InitializeComponent();
 		GameType = gameType;
+        Level = level;
 		BindingContext = this;
 
-		GenerateQuestion();
+		//GenerateQuestion();
 	}
 
-	private void GenerateQuestion()
-	{
-
-	}
-
-	private void OnAnswerSubmitted(object sender, EventArgs e)
+    private void OnAnswerSubmitted(object sender, EventArgs e)
     {
 
     }
