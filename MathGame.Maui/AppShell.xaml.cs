4dfe6dbd2f0a968c5a628eb3fbdd9304ed88cfab
COANAHEIM\JDelgado <jdelgado@anaheim.net>
2025-06-10 10:42:12 -0700

    Added Routing using the Shell

--- MathGame.Maui/AppShell.xaml.cs
+++ MathGame.Maui/AppShell.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace MathGame.Maui;
+﻿using MathGame.Maui.Views;
+
+namespace MathGame.Maui;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+
+        Routing.RegisterRoute("Menu", typeof(Menu));
+        Routing.RegisterRoute("Game", typeof(Game));
     }
 }
